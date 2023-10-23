1aaad96b60cd3d64f665224eb469817e2051db0c
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-23 17:33:04 +0200

    feat : show filtered recipes #9

---
+++ src/controllers/filteredRecipes.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Application\Controllers\AllRecipes;
+
+require_once('src/lib/database.php');
+require_once('src/model/filteredRecipes.php');
+
+use Application\Lib\Database\DatabaseConnection;
+use Application\Model\FilteredRecipes\FilteredRecipesRepository;
+
+class AllRecipes {
+    public function execute(string $type, string $option) {
+        $recipes = [];
+        $allRecipeRepository = new filteredRecipesRepository(new DatabaseConnection());
+
+        switch ($type) {
+            case 'categoriy':
+                $recipes = $allRecipeRepository->getFilteredRecipesByCategory(intval($option));
+                break;
+            case 'title':
+                $recipes = $allRecipeRepository->getFilteredRecipesByTitle($option);
+                break;
+            case 'ingredient':
+                $recipes = $allRecipeRepository->getFilteredRecipesByIngredient(intval($option));
+                break;
+        }
+
+        require('templates/filteredRecipes.php');
+    }
+}
---
+++ templates/filteredRecipes.php
@@ -0,0 +1,15 @@
+<?php $title = "Filtered recipes"; ?>
+
+<?php ob_start(); ?>
+<h1>All recipes</h1>
+
+<!-- Show all recipes -->
+<?php foreach ($recipes as $recipe): ?>
+<div>
+    <h2><?= $recipe->rec_title ?></h2>
+</div>
+<?php endforeach; ?>
+
+<?php $content = ob_get_clean(); ?>
+
+<?php require('layout.php') ?>
