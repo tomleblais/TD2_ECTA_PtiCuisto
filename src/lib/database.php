cd0e04d37b65e748de82cad713dcd9e8931b1272
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-26 10:39:03 +0200

    database complete #3

--- src/lib/database.php
+++ src/lib/database.php
@@ -7,9 +7,21 @@ class DatabaseConnection {
 
     public function getConnection(): \PDO {
         if ($this->database === null) {
-            $this->database = new \PDO('lien;dbname=nom;charset=utf8', 'blog', 'password');
+            $host = getenv('DB_HOST');
+            $dbname = getenv('DB_NAME');
+            $user = getenv('DB_USER');
+            $pass = getenv('DB_PASS');
+
+            try {
+                $this->database = new \PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+                $this->database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+            } catch (\PDOException $error) {
+                die("Erreur de connexion à la base de données : " . $error->getMessage());
+            }
         }
 
         return $this->database;
     }
 }
+
+// $this->database = new \PDO('lien;dbname=nom;charset=utf8', 'blog', 'password');
--- templates/homepage.php
+++ templates/homepage.php
@@ -3,6 +3,8 @@
 <?php ob_start(); ?>
 <h1>Petit cuisto</h1>
 
+<a href="index.php?action=allRecipes"></a>
+
 <?php $content = ob_get_clean(); ?>
 
 <?php require('layout.php') ?>
