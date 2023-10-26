9cd158bc578828457380c7bd34ee651b9a906033
Alexandre Lerosier <alex.lerosier@gmail.com>
2023-10-26 15:41:38 +0200

    feat : test data connection #3

--- src/lib/database.php
+++ src/lib/database.php
@@ -7,20 +7,49 @@ class DatabaseConnection {
 
     public function getConnection(): \PDO {
         if ($this->database === null) {
+            $this->loadEnv();
+
             $host = getenv('DB_HOST');
+            $port = getenv('DB_PORT');
             $dbname = getenv('DB_NAME');
             $user = getenv('DB_USER');
             $pass = getenv('DB_PASS');
 
+            echo "host = $host <br>";
+            echo "port = $port <br>";
+            echo "dbname = $dbname <br>";
+            echo "user = $user <br>";
+            echo "pass = $pass <br>";
+
             try {
-                $this->database = new \PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
-                $this->database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                echo "mysql:host=$host:$port;dbname=$dbname, $user, $pass <br>";
+                $this->database = new \PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $user, $pass);
+                //$this->database->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
             } catch (\PDOException $error) {
                 die("Erreur de connexion à la base de données : " . $error->getMessage());
             }
-        }
+            echo "connexion pdo";
+        }        
 
         return $this->database;
+    }
+    
+    private function loadEnv() {
+        $envFile = './.env';
+
+        if (file_exists($envFile)) {
+            $env = parse_ini_file($envFile);
+
+            if ($env !== false) {
+                foreach ($env as $key => $value) {
+                    putenv("$key=$value");
+                }
+            } else {
+                die("Erreur lors de la lecture du fichier .env");
+            }
+        } else {
+            die("Le fichier .env n'existe pas.");
+        }        
     }
 }
 
