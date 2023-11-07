86e587e9d44b6d4412b0e4c3030b12080780ca59
Ethan <ethanleneindre@etu.unicaen.fr>
2023-11-07 11:21:10 +0100

    feat: Build viewRecipe and comments #7 #11

--- src/controllers/user/viewRecipe.php
+++ src/controllers/user/viewRecipe.php
@@ -8,7 +8,10 @@ use Application\Model\User\ViewRecipe\ViewRecipeModel;
 
 class ViewRecipe {
     public function execute(int $id) {
-        $viewRecipeModel = new ViewRecipeModel();
+        $viewRecipeModel = new ViewRecipeModel($id);
+
+        $recipe= $viewRecipeModel->getRecipe();
+        $comments = $viewRecipeModel->getComments();
 
         require('templates/user/viewRecipe.php');
     }
--- templates/user/viewRecipe.php
+++ templates/user/viewRecipe.php
@@ -1,8 +1,46 @@
 <?php $title = "recette"; ?>
 
-<?php ob_start(); ?>
+<?php
+ob_start();
+$date;
 
-<!-- Contenue de la page -->
+if ($recette->rec_modification_date === "0000-00-00 00:00:00") {
+    $date = $recette->rec_creation_date;
+} else {
+    $date = $recette->rec_modification_date;
+}
+?>
+
+<div>
+    <div>
+        <img src="<?= $recette->rec_image ?>" alt="Image de la recette en question" />
+        <div>
+            <?php foreach ($tags as $tag) : ?>
+                <span><?= $tag ?></span>
+            <?php endforeach; ?>
+        </div>
+    </div>
+    <div>
+        <div>
+            <div>
+                <h2><?= $recette->rec_title ?></h2>
+            </div>
+            <div>
+                <div><?= $recette->use_nickname ?></div>
+                <div><?= $date ?></div>
+            </div>
+        </div>
+        <p><?= $recette->rec_summary ?></p>
+    </div>
+</div>
+
+<?php foreach ($comments as $comment) : ?>
+    <div>
+        <h3><?= $comment->use_nickname ?></h3>
+        <p><?= $comment->com_date ?></p>
+        <p><?= $comment->com_content ?></p>
+    </div>
+<?php endforeach; ?>
 
 <?php $content = ob_get_clean(); ?>
 
