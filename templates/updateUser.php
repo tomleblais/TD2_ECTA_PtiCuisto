61a0e6ea1646833fa8b2fc88824cc3c1586dfc41
tomleblais <leblaistom@gmail.com>
2023-11-12 21:06:32 +0100

    fix: update account and update password #10

--- templates/updateUser.php
+++ templates/updateUser.php
@@ -4,7 +4,7 @@
 
     <h1>Mettre à jour mes informations</h1>
 
-    <?php if(isset($error) && $error !== "") : ?>
+    <?php if(isset($error) && $error !== "") :  ?>
         <p><?= $error ?></p>
     <?php endif; ?>
 
@@ -13,19 +13,19 @@
         <table>
             <tbody>
                 <tr>
-                    <td><label for="nickname">Nom d'utilisateur :<?= $user->use_nickname ?></label></td>
+                    <td><label for="nickname">Nom d'utilisateur : <?= $user->use_nickname ?></label></td>
                     <td><input type="text" name="nickname" id="nickname" value="<?= $user->use_nickname ?>" maxlength="32" required> </td>
                 </tr>
                 <tr>
-                    <td><label for="firstname">Prénom :<?= $user->use_firstname ?> </label></td>
+                    <td><label for="firstname">Prénom : <?= $user->use_firstname ?> </label></td>
                     <td><input type="text" name="firstname" id="firstname" value="<?= $user->use_firstname ?>" maxlength="32" required> </td>
                 </tr>
                 <tr>
-                    <td><label for="lastname">Nom :<?= $user->use_lastname ?> </label></td>
+                    <td><label for="lastname">Nom : <?= $user->use_lastname ?> </label></td>
                     <td><input type="text" name="lastname" id="lastname" value="<?= $user->use_lastname ?>" maxlength="32" required> </td>
                 </tr>
                 <tr>
-                    <td><label for="email">Email :<?= $user->use_email ?> </label></td>
+                    <td><label for="email">Email : <?= $user->use_email ?> </label></td>
                     <td><input type="email" name="email" id="email" value="<?= $user->use_email ?>" maxlength="32" required> </td>
                 </tr>
             </tbody>
@@ -42,15 +42,15 @@
         <table>
             <tbody>
                 <tr>
-                    <td><label for="oldpassword">Mot de passe actuel :</label></td>
+                    <td><label for="oldpassword">Mot de passe actuel : </label></td>
                     <td><input type="password" name="oldpassword" id="oldpassword" maxlength="32" required> </td>
                 </tr>
                 <tr>
-                    <td><label for="newpassword">Nouveau mot de passe :</label></td>
+                    <td><label for="newpassword">Nouveau mot de passe : </label></td>
                     <td><input type="password" name="newpassword" id="newpassword" maxlength="32" required> </td>
                 </tr>
                 <tr>
-                    <td><label for="newpassword2">Confirmer votre nouveau mot de passe :</label></td>
+                    <td><label for="newpassword2">Confirmer votre nouveau mot de passe : </label></td>
                     <td><input type="password" name="newpassword2" id="newpassword2" maxlength="32" required> </td>
                 </tr>
             </tbody>
